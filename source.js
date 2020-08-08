235fc662487728bf0c2512ac8a3a333fe4b53947
BootLegAidan <30330512+BootLegAidan@users.noreply.github.com>
2020-08-07 21:33:10 -0500

    Add files via upload

---
+++ source.js
@@ -0,0 +1,22 @@
+var input = document.getElementById("in");
+var output = document.getElementById("out");
+var button = document.getElementById("btn");
+var alert = document.getElementById("alrt");
+var temp = "";
+var letter = "";
+
+
+button.onclick = function() {
+	output.innerHTML = "";
+	temp = "";
+  emoji.replace_mode = 'unified';
+emoji.allow_native = true;
+  output.innerHTML = emoji.replace_colons(input.value.replace(/ /g, ' :clap: '));
+	output.innerHTML = output.innerHTML.split("\\").join("");
+	output.select();
+	document.execCommand("copy");
+	input.select();
+	alert.innerHTML = "Copied To Clipboard";
+};
+
+input.select();
